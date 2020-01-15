4fe7443bf6be15dcb6991113520c737399ab64f4
Kamil <peoner.nn@gmail.com>
2020-01-16 00:36:00 +0300

    something changed ... 20 days ago

--- jtag.srcs/sim_1/new/TestB.v
+++ jtag.srcs/sim_1/new/TestB.v
@@ -22,7 +22,7 @@
 
 module TestB(
     );
-    reg TMS = 1, TCK=0,TRST,TDI;
+    reg TMS = 1, TCK=0,TRST,TDI = 1'bz;
     reg CLK=0;
     wire TDO;//data output
     
@@ -71,6 +71,19 @@ initial begin
      end
 end
 
+event SET_TDI_z_triger;
+event SET_TDI_z_done_triger;
+
+initial begin 
+    forever begin
+     @(SET_TDI_z_triger);
+     @(negedge TCK);
+     TDI = 1'bz;
+     @(posedge TCK);
+     -> SET_TDI_z_done_triger;
+     end
+end
+
 event SET_TMS_1_triger;
 event SET_TMS_1_done_triger;
 
@@ -101,7 +114,8 @@ integer cur_state = 0;
 
 localparam STATE_RESET    = 0;
 localparam STATE_TEST_RUN = 1;
-localparam STATE_UPDATE   = 2;
+localparam STATE_SELECT   = 2;
+localparam STATE_UPDATE   = 3;
     /*localparam STATE_SELECT_IR_SCAN   =     4'd4;
     localparam STATE_DR_CAPTURE       =     4'd5;
     localparam STATE_DR_SHIFT         =     4'd6;
@@ -124,6 +138,11 @@ initial begin
         @(SET_TMS_0_done_triger);
         cur_state = STATE_TEST_RUN;
      end
+     if(cur_state == STATE_TEST_RUN || cur_state == STATE_UPDATE) begin
+         -> SET_TMS_1_triger;
+         @(SET_TMS_1_done_triger);
+         cur_state = STATE_SELECT;
+      end
      SET_SELECT_SCAN_ev = 0;
      -> SET_SELECT_SCAN_done_triger;
     end
@@ -131,7 +150,8 @@ end
 
 integer TDI_SHIFT_SIZE = 0;
 integer TDI_SHIFT_REG_I = 0;
-reg [31:0] TDI_SHIFT_REG;
+reg [31:0] TDI_SHIFT_REG = 0;
+reg [31:0] TDO_SHIFT_REG = 0;
 
 event SHIFT_TDI_triger;
 event SHIFT_TDI_done_triger;
@@ -150,67 +170,89 @@ initial begin
             else begin
                 -> SET_TDI_0_triger;
                 @(SET_TDI_0_done_triger);
-            end            
+            end
+                        
         end
         
-        if(TDI_SHIFT_REG[TDI_SHIFT_REG_I]) begin
-            -> SET_TDI_1_triger;
-        end
-        else begin
-            -> SET_TDI_0_triger;
-        end
+    if(TDI_SHIFT_REG[TDI_SHIFT_REG_I]) begin
+        -> SET_TDI_1_triger;
+    end
+    else begin
+        -> SET_TDI_0_triger;
+    end
+    TDI_SHIFT_REG_I = TDI_SHIFT_REG_I + 1;
         
-        @(negedge TCK);
-        -> SHIFT_TDI_done_triger;
-        SHIFT_TDI_ev = 0;        
+    //@(negedge TCK);
+    
+    -> SHIFT_TDI_done_triger;
+    SHIFT_TDI_ev = 0;        
     end
 end
 
-event LS_DR_triger; //SDR
-event LS_DR_done_triger;
-reg LS_DR_ev = 0;
+initial begin 
+    forever begin
+     @(SHIFT_TDI_triger);
+     TDO_SHIFT_REG = 0;
+     SHIFT_TDI_ev = 1;
+     for (TDI_SHIFT_REG_I = 0; TDI_SHIFT_REG_I < TDI_SHIFT_SIZE; ) 
+        begin
+            @(posedge TCK);
+            TDO_SHIFT_REG[TDI_SHIFT_REG_I] = TDO;
+            @(negedge TCK);
+        end        
+    end
+end
+
+
+
+
+
+
+event LSDR_triger; //SDR
+event LSDR_done_triger;
+reg LSDR_ev = 0;
 
 initial begin 
     forever begin
-     @(LS_DR_triger);
-     LS_DR_ev = 1;
+     @(LSDR_triger);
+     LSDR_ev = 1;
      -> SET_SELECT_SCAN_triger;
      @(SET_SELECT_SCAN_done_triger);
-     
-     -> SET_TMS_1_triger;
-     @(SET_TMS_1_done_triger);
      
      -> SET_TMS_0_triger;
      @(SET_TMS_0_done_triger);
      
      -> SET_TMS_0_triger;
-     //@(SET_TMS_0_done_triger);
+     @(SET_TMS_0_done_triger);
      
      -> SHIFT_TDI_triger;
      @(SHIFT_TDI_done_triger)
+
+     -> SET_TMS_1_triger;
+     //shift -> exit 
+     @(SET_TMS_1_done_triger);
+     
+     -> SET_TDI_z_triger;
      
      -> SET_TMS_1_triger;
      //exit -> update
      @(SET_TMS_1_done_triger);     
      cur_state = STATE_UPDATE;
-     -> LS_DR_done_triger;
-     LS_DR_ev = 0;
+     -> LSDR_done_triger;
+     LSDR_ev = 0;
      end
 end
 
-event LS_IR_triger;//SIR
-event LS_IR_done_triger;
-reg LS_IR_ev = 0;
+event LSIR_triger;//SIR
+event LSIR_done_triger;
+reg LSIR_ev = 0;
 
 initial begin 
     forever begin
-    @(LS_IR_triger);
-    LS_IR_ev = 1;
+    @(LSIR_triger);
+    LSIR_ev = 1;
     -> SET_SELECT_SCAN_triger;
     @(SET_SELECT_SCAN_done_triger);
-    
-    -> SET_TMS_1_triger;
-    @(SET_TMS_1_done_triger);
     
     -> SET_TMS_1_triger;
     @(SET_TMS_1_done_triger);
@@ -219,17 +261,23 @@ initial begin
     @(SET_TMS_0_done_triger);
     
     -> SET_TMS_0_triger;
-    //@(SET_TMS_0_done_triger);
+    @(SET_TMS_0_done_triger);
     
     -> SHIFT_TDI_triger;
     @(SHIFT_TDI_done_triger)
     
     -> SET_TMS_1_triger;
+     //shift -> exit 
+     @(SET_TMS_1_done_triger);
+     
+     -> SET_TDI_z_triger;
+         
+    -> SET_TMS_1_triger;
     //exit -> update
     @(SET_TMS_1_done_triger);     
     cur_state = STATE_UPDATE;
-    -> LS_IR_done_triger;
-    LS_IR_ev = 0;
+    -> LSIR_done_triger;
+    LSIR_ev = 0;
     end
 end
 
@@ -243,23 +291,61 @@ begin
     #8
     SW[8]=0;
 end
-   
+
+    localparam EXTEST         = 4'b0000;
+    localparam SAMPLE_PRELOAD = 4'b0010;
+    localparam IDCODE         = 4'b0001;
+    localparam INTEST         = 4'b0100;
+    localparam BYPASS         = 4'b1111;
+
+reg [4:0] test_id;   
 initial
     
     begin
         # 3
+        
+        //TDI_IR_test
+//////////////////////////////////////////////////////////////
+        test_id = 1;
         TDI_SHIFT_SIZE = 4;
-        TDI_SHIFT_REG  = 4'b0001;
+        TDI_SHIFT_REG  = 4'b1001;
         
-        -> LS_IR_triger;
-        @(LS_IR_done_triger);
+        -> LSIR_triger;
+        @(LSIR_done_triger);
         
+        //IDCODE TEST
+//////////////////////////////////////////////////////////////
+        test_id = 2;
         TDI_SHIFT_SIZE = 4;
-        TDI_SHIFT_REG  = 4'b1111;
+        TDI_SHIFT_REG  = IDCODE;
         
-        -> LS_IR_triger;
-        @(LS_IR_done_triger);
+        -> LSIR_triger;
+        @(LSIR_done_triger);
+        
+        
+        TDI_SHIFT_SIZE = 32;
+        TDI_SHIFT_REG  = 0;
+        
+        -> LSDR_triger;
+        @(LSDR_done_triger);
 
+        // sample preload
+//////////////////////////////////////////////////////////////
+        test_id = 3;
+        TDI_SHIFT_SIZE = 4;
+        TDI_SHIFT_REG  = SAMPLE_PRELOAD;
+        
+        -> LSIR_triger;
+        @(LSIR_done_triger);        
+
+        TDI_SHIFT_SIZE = 18;
+        TDI_SHIFT_REG  =  0;
+        
+        -> LSDR_triger;
+        @(LSDR_done_triger);        
+
+//////////////////////////////////////////////////////////////
+        test_id = 4;
         /*TDI = 1;
         TCK = 0; 
         TMS = 1;
